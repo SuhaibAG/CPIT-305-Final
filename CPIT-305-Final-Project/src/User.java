775d29a020554cbfea4933ccaef40185bb50a2bd
Suhaib Alghamdi <suhaib.ag.49@gmail.com>
2023-11-27 13:44:42 +0300

    Add files via upload

---
+++ CPIT-305-Final-Project/src/User.java
@@ -0,0 +1,42 @@
+public class User {
+    private int id;
+    private String username;
+
+    public User(int id, String username) {
+        //if the user is in the database fetch info through the database
+        this.id = id;
+        this.username = username;
+
+    }
+
+    public void makeRequest(String user, int amount, boolean give){
+        //sql code to make a request if the boolean give is false then the user is requesting money
+
+        //sql code to make a request if the boolean give is true then the user is giving money
+    }
+
+    public void removeRequest(String username){
+        //sql code to remove all requests by username
+    }
+
+    public void viewRequests(){
+        //sql code to view all requests and print them in method
+    }
+
+    public void acceptRequests(String username){
+        //sql code to accept the request
+    }
+
+    public void giveMoney(int amount){
+        //code using sql to give money
+    }
+
+    public void owedList(){
+        //code using sql to retrieve all users who owe the user money
+    }
+
+    public String owingList(){
+        //code using sql retrieve all users who the user owes money to
+    }
+
+}
