caa2b3eed845bf34f64c1185f62dceb0551302d0
Suhaib Alghamdi <suhaib.ag.49@gmail.com>
2023-11-28 13:10:58 +0300

    added logout

--- CPIT-305-Final-Project/src/Client.java
+++ CPIT-305-Final-Project/src/Client.java
@@ -17,8 +17,11 @@ public class Client {
             Scanner userInput = new Scanner(System.in);
 
             boolean login = false;
+            User user;
             while (true) {
                 String message = "";
+
+                //login condition
                 if (!login) {
                     System.out.println("enter your username");
                     message = "0" + userInput.next();
@@ -27,8 +30,25 @@ public class Client {
 
                     System.out.println(response);
                     if(response.equals("logged in")){
+                        user = new User(0, message.substring(1));
                         login = true;
                     }
+                }
+
+                else{
+                    int command = printcommands(userInput);
+
+                    //log out condition
+                    if (command == 0){
+                        login = false;
+                    }
+
+                    //command interpreter
+                    else if (command > 1 && command < 9){
+
+                    }
+
+
                 }
 
 
@@ -49,8 +69,24 @@ public class Client {
         }
     }
 
-    public static void printcommands(Scanner in){
-        String message = in.nextLine();
-        System.out.printf(message);
+    public static int printcommands(Scanner userInput){
+        boolean done = false;
+        System.out.println("Enter 1 if you want to do thing");
+        System.out.println("Enter 2 if you want to do thing");
+        System.out.println("Enter 3 if you want to do thing");
+        System.out.println("Enter 4 if you want to do thing");
+        System.out.println("Enter 5 if you want to do thing");
+        System.out.println("Enter 6 if you want to do thing");
+        System.out.println("Enter 7 if you want to do thing");
+        System.out.println("Enter 8 if you want to log out");
+        int command = -1;
+        while(!done){
+            command = userInput.nextInt();
+            if (command > 0 && command < 9){
+                done = true;
+            }
+        }
+        return command;
+
     }
 }
